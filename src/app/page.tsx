f7fcbf6635fbca8c090be793d111f8eff71c80f1
Dayif <sekoudayifourouk@gmail.com>
2025-01-09 17:11:16 +0000

    enlèvement de bizarrerie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default function Home() {
-  return <div className=" bg-slate-100 text-black z-10">Pagejnvez j e</div>;
+  return <div className=" bg-slate-100 text-black z-10"></div>;
 }
