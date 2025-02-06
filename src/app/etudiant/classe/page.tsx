874f8c84bcfec56424719a7e47662dc8086ab71c
Dayif <sekoudayifourouk@gmail.com>
2025-02-06 14:42:53 +0000

    mis à jour des calendars

--- src/app/etudiant/classe/page.tsx
+++ src/app/etudiant/classe/page.tsx
@@ -2,14 +2,14 @@
 import SmallIconCard from "@/component/smallIconCard/SmallIconCard";
 // import UserCard from "@/component/userCard/UserCard";
 // import "@fortawesome/fontawesome-free/css/all.min.css";
-import Calendar from "@/component/calendar/Calendar";
+import Calendrier from "@/component/calendrier/Calendrier";
 import TableauD from "@/component/tableau/TableauD";
 
 const Classepage = () => {
   return (
-    <div>
-      <div className=" float-right m-10 bg-white shadow-md rounded p-6 w-72">
-        <Calendar />
+    <div className="pt-16">
+      <div className="float-right flex justify-center gap-4 bg-white rounded border-[1px] border-gray-300">
+        <Calendrier />
       </div>
       <div className="flex justify-center p-10 space-x-4">
         <SmallIconCard
--- src/app/etudiant/messages/page.tsx
+++ src/app/etudiant/messages/page.tsx
@@ -1,14 +1,14 @@
 "use client";
+import Calendrier from "@/component/calendrier/Calendrier";
 import SmallIconCard from "@/component/smallIconCard/SmallIconCard";
 // import UserCard from "@/component/userCard/UserCard";
 // import "@fortawesome/fontawesome-free/css/all.min.css";
-import Calendar from "@/component/calendar/Calendar";
 
 const ClasseMessage = () => {
   return (
     <div>
       <div className=" float-right m-10 bg-white shadow-md rounded p-6 w-72">
-        <Calendar />
+        <Calendrier />
       </div>
       <div className="flex justify-center p-10 space-x-4">
         <SmallIconCard
